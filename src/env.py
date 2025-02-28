aa3310cc73fb91ff81404e2f1632d4bb1a58f284
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-02-28 08:11:19 +0400

    Add chrome driver connection

--- src/env.py
+++ src/env.py
@@ -1,7 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
 
 class DinoGame:
-    def __init__():
-        pass
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("disable-infobars")
+        self._driver = webdriver.Chrome(options=chrome_options)
+        self._driver.set_window_position(x=-10,y=0)
+        self._driver.set_window_size(200, 300)
+        self._driver.get(f"file://{os.path.abspath('src/dino_game/dino.html')}")  # Ensure it’s a file URL
+        self._driver.execute_script("Runner.config.ACCELERATION=0")
 
     def is_crashed(self):
         pass
