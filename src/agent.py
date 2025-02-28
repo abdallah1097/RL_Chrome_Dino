4ac713ba857985c75852830850999245222da594
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-02-28 08:19:53 +0400

    Add jump and duck methods

--- src/agent.py
+++ src/agent.py
@@ -4,10 +4,21 @@ class QLearningAgent:
     def __init__(self, game_env):
         self._game_env = game_env
 
-
     def start_game(self):
         """
         Starts the game by jumping once
         """
         self._game_env.jump()
         time.sleep(.5)
+
+    def is_running(self):
+        return self._game_env.get_playing()
+
+    def is_crashed(self):
+        return self._game_env.get_crashed()
+
+    def jump(self):
+        self._game_env.jump()
+
+    def duck(self):
+        self._game_env.duck()
