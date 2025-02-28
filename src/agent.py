4f2ae4ffe9cff6e9f1297ffabf5092f888c4d03f
abdallah1097 <abdallah.wallyallah97@gmail.com>
2025-02-28 08:17:33 +0400

    Add game env helper methods

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,13 @@
+import time
+
 class QLearningAgent:
-    def __init__(self):
-        pass
+    def __init__(self, game_env):
+        self._game_env = game_env
+
+
+    def start_game(self):
+        """
+        Starts the game by jumping once
+        """
+        self._game_env.jump()
+        time.sleep(.5)
